fcc228895347bb93baaaa97c7e270aa9e702f2cc
Kitsai <luck3801@gmail.com>
2025-02-27 22:16:40 -0300

    update image.ppm, main.cc, material.h and vec3.h

--- rt-weekend/main.cc
+++ rt-weekend/main.cc
@@ -36,12 +36,14 @@ int main() {
 
     auto material_ground = std::make_shared<lambertian>(color(.8, .8, 0));
     auto material_center = make_shared<lambertian>(color(.1, .2, .5));
-    auto material_left = make_shared<metal>(color(.8, .8, .8), .3);
+    auto material_left = make_shared<dielectric>(1.50);
+    auto material_bubble = make_shared<dielectric>(1.00/1.50);
     auto material_right = make_shared<metal>(color(.8, .6, .2), 1.0);
 
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100, material_ground));
     world.add(make_shared<sphere>(point3(0, 0, -1.2), 0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0, 0, -1.2), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0, 0, -1.2), 0.4, material_bubble));
     world.add(make_shared<sphere>(point3(1.0, 0, -1.2), 0.5, material_right));
 
     camera cam;
